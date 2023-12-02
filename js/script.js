37e4628cb661a1e4400fd806a44eb98ce6a83d59
DavidAdrian8ag <krezitech@hotmail.com>
2023-12-01 19:28:55 -0600

    New Portfolio

--- js/script.js
+++ js/script.js
@@ -1,128 +1,115 @@
-$(function(){
-	"use strict";
-	/*=========================================================================
-		Initializing stellar.js Plugin
-	=========================================================================*/
-	$('.section').stellar({
-		horizontalScrolling: false
-	});
-	$(window).on('load', function(){
-		$('body').addClass('loaded');
-		/*=========================================================================
-			Portfolio Grid
-		=========================================================================*/
-		var grid = $('#portfolio-grid');
-		grid.shuffle({
-			itemSelector: '.item'
-		});
-		$('#portfolio-filters > ul > li > a').on('click', function (e) {
-			e.preventDefault();
-			var groupName = $(this).attr('data-group');
-			$('#portfolio-filters > ul > li > a').removeClass('active');
-			$(this).addClass('active');
-			grid.shuffle('shuffle', groupName );
-		});
-		$('a.image-link').magnificPopup({
-			type: 'image',
-			removalDelay: 300,
-			mainClass: 'mfp-fade',
-			gallery: {
-				enabled: true
-			}
-		});
-	});
-	/*=========================================================================
-		Links Navigation System
-	=========================================================================*/
-	$('.front-person-links > ul > li > a[data-section]').on('click', function(e){
-		e.preventDefault();
-		var section = $('#' + $(this).data('section'));
-		if( section.size() != 0 ){
-			$('body').addClass('section-show');
-			section.addClass('active');
-		}
-	});
-	$('.close-btn').on('click', function(){
-		$('body').removeClass('section-show');
-		$('section.active').removeClass('active');
-	});
-	/*=========================================================================
-		Testimonials Slider
-	=========================================================================*/
-	$('.testimonials-slider').owlCarousel({
-		singleItem: true
-	});
-	/*=========================================================================
-		Skill Bar's Percent Initialization from attribute data-percent
-	=========================================================================*/
-	$('.skill-bar').each(function(){
-		var $this = $(this),
-			percent = parseInt( $this.data('percent'), 10 );
-		$this.find('.bar').css('width', percent + '%');
-	});
-	/*=========================================================================
-		Contact Form
-	=========================================================================*/
-	function isJSON(val){
-		var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
-		return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
-	}
-	$('#contact-form').validator().on('submit', function (e) {
-		if (!e.isDefaultPrevented()) {
-			// If there is no any error in validation then send the message
-			e.preventDefault();
-			var $this = $(this),
-				//You can edit alerts here
-				alerts = {
-					success:
-					"<div class='form-group' >\
-						<div class='alert alert-success alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
-						</div>\
-					</div>",
-					error:
-					"<div class='form-group' >\
-						<div class='alert alert-danger alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Error!</strong> Sorry, an error occurred. Try again.\
-						</div>\
-					</div>"
-				};
-			$.ajax({
-				url: 'mail.php',
-				type: 'post',
-				data: $this.serialize(),
-				success: function(data){
-					if( isJSON(data) ){
-						data = $.parseJSON(data);
-						if(data['error'] == false){
-							$('#contact-form-result').html(alerts.success);
-							$('#contact-form').trigger('reset');
-						}else{
-							$('#contact-form-result').html(
-							"<div class='form-group' >\
-								<div class='alert alert-danger alert-dismissible' role='alert'> \
-									<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-										<i class='ion-ios-close-empty' ></i> \
-									</button> \
-									"+ data['error'] +"\
-								</div>\
-							</div>"
-							);
-						}
-					}else{
-						$('#contact-form-result').html(alerts.error);
-					}
-				},
-				error: function(){
-					$('#contact-form-result').html(alerts.error);
-				}
-			});
-		}
-	});
-});
+(function ($) {
+    "use strict";
+    $(".history-scroller").niceScroll({
+        cursorwidth: "10px",
+        background: "#0d1015",
+        cursorborder: "0",
+        cursorborderradius: "0",
+        autohidemode: false,
+        zindex: 5
+    });
+    // testimonial-slider
+    $('.testimonials').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        arrows: false,
+        adaptiveHeight: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+    animatedProgressBar();
+    windowHieght();
+    previewPannel();
+    function animatedProgressBar() {
+        $(".progress").each(function () {
+            var skillValue = $(this).find(".skill-lavel").attr("data-skill-value");
+            $(this).find(".bar").animate({
+                width: skillValue
+            }, 1500);
+            $(this).find(".skill-lavel").text(skillValue);
+        });
+    }
+    function windowHieght() {
+        if ($(window).height() <= 768) {
+            $(".pt-table").addClass("desktop-768");
+        } else {
+            $(".pt-table").removeClass("desktop-768");
+        }
+    }
+    /*----------------------------------------
+        Isotope Masonry
+    ------------------------------------------*/
+    function isotopeMasonry() {
+        $(".isotope-gutter").isotope({
+            itemSelector: '[class^="col-"]',
+            percentPosition: true
+        });
+        $(".isotope-no-gutter").isotope({
+            itemSelector: '[class^="col-"]',
+            percentPosition: true,
+            masonry: {
+                columnWidth: 1
+            }
+        });
+        $(".filter a").on("click", function () {
+            $(".filter a").removeClass("active");
+            $(this).addClass("active");
+            var selector = $(this).attr("data-filter");
+            $(".isotope-gutter").isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 750,
+                    easing: "linear",
+                    queue: false
+                }
+            });
+            return false;
+        });
+    }
+    /*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+        Preview Pannel
+    -=-=-=-=-=-=-=-=-=--=-=-=-=-=-*/
+    function previewPannel() {
+        $(".switcher-trigger").on("click", function () {
+            $(".preview-wrapper").toggleClass("extend");
+            return false;
+        });
+        if ($(window).width() < 768) {
+            //$(".preview-wrapper").removeClass("extend");
+        }
+        $(".color-options li").on("click", function () {
+            $("#color-changer").attr({
+                "href": "css/colors/" + $(this).attr("data-color") + ".css"
+            });
+            return false;
+        });
+    }
+    $(window).on("load", function () {
+        isotopeMasonry();
+        $(".preloader").addClass("active");
+        setTimeout(function () {
+            $(".preloader").addClass("done");
+        }, 1000);
+    });
+})(jQuery);
